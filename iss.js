fb400cc222e677adbdb86297a3f137bc69129b54
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-25 20:03:36 +0000

    Added fetch coordinates by IP

--- iss.js
+++ iss.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require('request');
 
 // fetch IP address
 const fetchMyIp = function(callback) {
@@ -12,9 +12,35 @@ const fetchMyIp = function(callback) {
       callback(Error(msg), null);
       return;
     }
-    const ip = JSON.parse(body).ip
-    callback(null, ip)
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   })
 }
 
-module.exports = { fetchMyIp }
+const fetchCoordsByIP = function (ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+  if (error) {
+    callback(error, null);
+    return;
+  }
+
+  const parsedBody = JSON.parse(body);
+
+  if (!parsedBody.success) {
+    const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+    callback(Error(message), null);
+    return;
+  } 
+
+  const { latitude, longitude } = parsedBody;
+
+  callback(null, {latitude, longitude});
+});
+}
+
+
+module.exports = { 
+  fetchMyIp,
+  fetchCoordsByIP
+ }
