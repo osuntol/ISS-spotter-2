d88eb014b0c8685fbba5e380baf531f623e1bb21
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-25 19:21:54 +0000

    New ISS file modification

---
+++ iss.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+// fetch IP address
+const fetchMyIp = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip
+    callback(null, ip)
+  })
+}
+
+module.exports = { fetchMyIp }
