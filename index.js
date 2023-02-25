d8e4f7e7d8f50cfa7f3dbfc1dbcbab451c7cfbea
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-25 20:26:15 +0000

    New function added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+const { fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
 
 // fetchMyIP ((error, ip) => {
 //   if (error) {
@@ -10,10 +11,25 @@ const { fetchCoordsByIP } = require('./iss');
 //   console.log("It worked! Returned IP:" . ip)
 // })
 
+let coords = {
+  latitude: '34.1083449',
+  longitude: '-117.2897652'
+}
+
+
 fetchCoordsByIP('76.86.127.30', (error, coordinates) => {
   if (error){
     console.log("It didnt work!", error)
     return;
   }
   console.log('It worked! Returned coordinates:' , coordinates)
+})
+
+
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+  if(error) {
+    console.log ("It didnt work!" , error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:' , passTimes);
 })
