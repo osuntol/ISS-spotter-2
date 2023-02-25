fc3c323ce1cf1f3b4c9a0159bbd61616c566b8e1
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-25 20:26:42 +0000

    New function added

--- iss.js
+++ iss.js
@@ -6,7 +6,6 @@ const fetchMyIp = function(callback) {
     if (error) {
       return callback(error, null);
     }
-    // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
@@ -17,30 +16,44 @@ const fetchMyIp = function(callback) {
   })
 }
 
-const fetchCoordsByIP = function (ip, callback) {
+const fetchCoordsByIP = function(ip, callback) {
   request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    const { latitude, longitude } = parsedBody;
+    callback(null, { latitude, longitude });
+  });
+}
 
-  if (error) {
-    callback(error, null);
-    return;
-  }
+fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
-  const parsedBody = JSON.parse(body);
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  })
 
-  if (!parsedBody.success) {
-    const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
-    callback(Error(message), null);
-    return;
-  } 
-
-  const { latitude, longitude } = parsedBody;
-
-  callback(null, {latitude, longitude});
-});
 }
 
 
-module.exports = { 
+module.exports = {
   fetchMyIp,
-  fetchCoordsByIP
- }
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
+}
