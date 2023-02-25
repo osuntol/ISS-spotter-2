da3a1e971f2e5158c414c51d5fa46a786e07ff27
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-25 20:02:59 +0000

    Added fetch coordinates by IP

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP ((error, ip) => {
-  if (error) {
-    console.log("It didnt work!", error);
+// fetchMyIP ((error, ip) => {
+//   if (error) {
+//     console.log("It didnt work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:" . ip)
+// })
+
+fetchCoordsByIP('76.86.127.30', (error, coordinates) => {
+  if (error){
+    console.log("It didnt work!", error)
     return;
   }
-
-  console.log("It worked! Returned IP:" . ip)
+  console.log('It worked! Returned coordinates:' , coordinates)
 })
